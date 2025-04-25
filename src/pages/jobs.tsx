b9b96b91a4250b8000d715f5f67a98f528ad4057
Martin Bjervig <martinbjervig@gmail.com>
2025-04-25 17:00:36 +0200

    added education

--- src/pages/jobs.tsx
+++ src/pages/jobs.tsx
@@ -18,7 +18,7 @@ export const Jobs = () => {
                 const data = await response.json();
                 setWorkData(data);
             } catch (error) {
-                console.error("f*n också, ni kan inte se mina jobb..fel med fetchen :D", error)
+                console.error("du kan inte se mina jobb..fel med fetchen :D", error)
             }
         };
         fetchData();
@@ -27,26 +27,26 @@ export const Jobs = () => {
     return (
         <div className="section">
             <h1 className="section-title">Arbetslivserfarenhet</h1>
-            {workData.map((job) => (
-                <div key={job.id} className="entry-card">
-                    <h2 className="entry-title">{job.companyName}</h2>
-                    <p className="entry-location">{job.location}</p>
-                    <p className="entry-dates">
-                        {Array.isArray(job["date&year"])
-                        ? job["date&year"].join(", ")
-                        : job["date&year"]}
-                    </p>
-                        {Array.isArray(job.description) ? (
-                            <ul className="entry-description">
-                                {job.description.map((desc, index) =>( 
-                                    <div key={index}>{desc}</div>
-                                ))}
-                            </ul>
-                    ) : (
-                        <p className="entry-description">{job.description}</p>
-                    )}
-                </div>
-            ))}
+                {workData.map((job) => (
+                    <div key={job.id} className="entry-card">
+                        <h2 className="entry-title">{job.companyName}</h2>
+                        <p className="entry-location">{job.location}</p>
+                        <p className="entry-dates">
+                            {Array.isArray(job["date&year"])
+                            ? job["date&year"].join(", ")
+                            : job["date&year"]}
+                        </p>
+                            {Array.isArray(job.description) ? (
+                                <ul className="entry-description">
+                                    {job.description.map((desc, index) =>( 
+                                        <div key={index}>{desc}</div>
+                                    ))}
+                                </ul>
+                        ) : (
+                            <p className="entry-description">{job.description}</p>
+                        )}
+                    </div>
+                ))}
         </div>
     );
 };
