ded784a22ff28e91a1ab068dcd1abb16ec03a1bd
Martin Bjervig <martinbjervig@gmail.com>
2025-04-25 12:10:52 +0200

    added routing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import {Routes,Route} from "react-router-dom";
-
-import './App.css'
+import { Home } from "./pages/home";
+import { Jobs } from "./pages/jobs";
+import { Education } from "./pages/education";
+import { Projects } from "./pages/projects";
+import { Contact } from "./pages/contact";
 
 function App() {
 
@@ -8,7 +11,11 @@ function App() {
   return (
     <>
     <Routes>
-      <Route></Route>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/jobs" element={<Jobs/>}/>
+      <Route path="/education" element={<Education/>}/>
+      <Route path="/projects" element={<Projects/>}/>
+      <Route path="/contact" element={<Contact/>}/>
     </Routes>
     </>
   )
